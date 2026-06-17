647f2ae60ce10e7c812014463dd68a0d4d10351e
Gopi <208a1a0543@gmail.com>
2026-06-17 16:23:49 +0530

    Connect frontend to Railway backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://nexthire-backend-production-b9c8.up.railway.app/api"
 });
 
 API.interceptors.request.use((config) => {
